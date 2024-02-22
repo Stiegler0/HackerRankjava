c6e508e9f32aec1661a331fe246fbd8525fd2205
Stiegler0 <yassinejemlaoui23@gmail.com>
2024-02-22 22:37:08 +0100

    init file bash: line 1: wq: command not found !

---
+++ Easy01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Easy01{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("============")
+		for(int i=0;i<3;i++) {
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			System.out.println(s1);
+			int len = 15 - s1.length();
+			for (int j = 0; j <= len; j++) {
+				System.out.println(" ");
+			}
+			String x1 = String.ValueOf(x);
+			if (s1.lentgh() == 3) {//
+			}
+		}
